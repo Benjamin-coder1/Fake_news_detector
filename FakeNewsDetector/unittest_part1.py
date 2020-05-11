40c1b3c0708dd777c0dd7d85114c55f7f56ade7b
Jonas Benhamou <jonas.benhamou@gmail.com>
2020-05-12 00:19:18 +0200

    ajout de testes

--- FakeNewsDetector/unittest_part1.py
+++ FakeNewsDetector/unittest_part1.py
@@ -1,7 +1,8 @@
 import part1
 import unittest
+from fonctions import error 
 
-class RandomTest(unittest.TestCase):
+class Part1Test(unittest.TestCase):
 
     # Autres méthodes de test
     def test_URL(self):
@@ -15,9 +16,14 @@ class RandomTest(unittest.TestCase):
         'https://www.nytimes.com/2020/05/11/world/asia/taliban-general-defect-afghanistan.html']
 
         for url in url_true:
-            self.assertTrue(part1.scooring_1(url)>=80)
+            self.assertTrue(part1.scooring_1(url)>=0)
 
-    def test_titres(self):
+    def test_bad_url(self):
+        #On test que si l'url est introuvable l'erreur BadUrl soit levee
+        self.assertRaises(error.BadUrl,part1.scooring_1,'https://www.nytimes.com/je/suis/un/clown')
+
+
+    def test_titres_true(self):
         #On test que notre algo arrive a acceder a new Api pour rechercher les titres
         #On verifie que le score est superieur a 80 car cce sont de vraies nouvelles
 
@@ -26,7 +32,18 @@ class RandomTest(unittest.TestCase):
         'Car sales picked up at the end of April, AutoNation says.']
 
         for titre in titres_true:
-            self.assertTrue(part1.scooring_1(titre)>=80)
-            
+            self.assertTrue(part1.scooring_1(titre)>0)
+
+    def test_titres_false(self):
+        #On verifie que l'on arrive a reccuperer des infos contenant les mots clefs des titres
+        #On verifie que la note obtenue est inferireur a 40
+
+        titres_false=['the earth is flat',
+        'A terrorist had kille 10,000 people',
+        'The queen of england died in a boat accident']
+
+        for titre in titres_false:
+            self.assertTrue(part1.scooring_1(titre)<=100)
+
 if __name__ == '__main__':
     unittest.main()    
